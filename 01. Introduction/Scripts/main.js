973e1b8277666a91b6524172e85ef99f740669c1
Vladislav Dekov <vdekoff@gmail.com>
2016-02-14 16:26:03 +0200

    The code for the introduction lecture.

---
+++ 01. Introduction/Scripts/main.js
@@ -0,0 +1,6 @@
+console.log("I am console.log,", "and again console.log,", "and again!");
+console.info("I am console.info");
+console.warn("I am console.warn");
+console.error("I am console.error");
+
+console.log( window );
