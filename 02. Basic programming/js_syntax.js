a178e29e0c3fb188195d72270793dd44855940a5
Vladislav Dekov <vdekoff@gmail.com>
2016-02-18 18:46:55 +0200

    Introdcution into the JavaScript basic syntax.

---
+++ 02. Basic programming/js_syntax.js
@@ -0,0 +1,18 @@
+// Single-line comment
+/* Multi-line
+comment */
+var operand = 2 + 5; // operand operator expression semicolon
+window.document;     // property reference
+window['document'];  // property reference
+
+if ( true ) {        // block
+   alert( 'true' );
+}
+
+function foo() {     // function declaration
+   return null;
+}
+
+var myvar = null;    // define variable & assign `null`
+var bar;             // declare a variable (value is `undefined`)
+window.foo;          // accessing unexisting property
