a09817f32ae60643df8a8f4f8310658da19b478d
Vladislav Dekov <vdekoff@gmail.com>
2016-04-07 11:24:50 +0300

    The slides from lecture for CSS manipulation.

--- Scripts/dom_api.js
+++ Scripts/dom_api.js
@@ -448,3 +448,68 @@ Object.defineProperty( HTMLElement.prototype, 'classList', {
       return classList;
    }
 });
+
+/**
+ * CSSStyleSheet.prototype.insertRule
+ *
+ * Creates new CSS rule ( selector { cssprop: cssvalue } )
+ *
+ * @param rule {String} The CSS rule to insert
+ *
+ * @returns {Number} The new rule's index
+**/
+! CSSStyleSheet.prototype.insertRule && (CSSStyleSheet.prototype.insertRule = function ( rule, index ) {
+   if ( this.addRule ) {
+      // extract the selector
+      var selector   = rule.substring( 0, rule.indexOf('{') );
+      // now extraxt the selector body
+      var style      = rule.substring( rule.indexOf('{') + 1, rule.lastIndexOf('}') );
+      // insert the rule!
+      return this.addRule( selector, style, index );
+   }
+   return -1;
+});
+
+/**
+ * CSSStyleSheet.prototype.deleteRule
+ *
+ * Removes a CSS rule from a stylesheet by specified index
+ *
+ * @param index {Number} The CSS rule's index
+ *
+ * @returns {Boolean}
+**/
+! CSSStyleSheet.prototype.deleteRule && (CSSStyleSheet.prototype.deleteRule = function ( index ) {
+   return this.removeRule ? this.removeRule( index ) : false;
+});
+
+/**
+ * CSSStyleSheet.prototype.cssRules
+ *
+ * Standardize `cssRules` property
+**/
+document.styleSheets[0] && ! document.styleSheets[0].cssRules && Object.defineProperty( CSSStyleSheet.prototype, 'cssRules', {
+   enumerable     : false,
+   configurable   : true,
+   
+   get : function () {
+      return this.rules;
+   }
+   
+});
+
+/**
+ * HTMLStyleElement.prototype.sheet
+ *
+ * Standardize `sheet` property
+**/
+'styleSheet' in document.createElement('style') && Object.defineProperty( HTMLStyleElement.prototype, 'sheet', {
+   
+   enumerable     : false,
+   configurable   : true,
+   
+   get : function () {
+      return this.styleSheet;
+   }
+   
+});
